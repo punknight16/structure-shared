15367fdb8d61d9d7800fce294e8fa2440235e7fe
Daniel Miller <punknight16@gmail.com>
2020-08-27 15:16:28 -0700

    comment out a function because 'navigator' is not defined

--- src/Common.ts
+++ src/Common.ts
@@ -1,11 +1,11 @@
 import * as _ from 'lodash'
 import * as Dynamo from './Dynamo';
 import * as AWSSdk from 'aws-sdk';
-
+/*
 export function IsElectron() {
 	var userAgent = navigator.userAgent.toLowerCase();
 	return (userAgent.indexOf(' electron/') > -1)
-}
+}*/
 
 export const max= (a, initialValue)=> {
 	return a.reduce((m,x)=> parseInt(m)>parseInt(x) ? parseInt(m):parseInt(x), initialValue);
